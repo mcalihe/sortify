5766bd093ba8e7ae449c71a6eed8e99a3ed4d5ca
Michael Isler <michael.isler@hotmail.ch>
2024-09-29 17:27:13 +0200

    initial setup of the angular frontend and spring backend

---
+++ backend/src/main/java/com/mcalihe/sortify/SortifyApplication.java
@@ -0,0 +1,13 @@
+package com.mcalihe.sortify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SortifyApplication.class, args);
+    }
+
+}
